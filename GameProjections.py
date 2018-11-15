bccae71aed9096f99059aa05e29757829edeb596
patrickwwbutler <patrick.butler77@gmail.com>
2018-11-15 10:34:50 -0500

    Created nextGameScore column

--- GameProjections.py
+++ GameProjections.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
 # Simple method to calculate fantasy points
@@ -11,7 +12,7 @@ def halfPPRFantasyPoints(row):
         total += float(row['ReceivingTDs'])*6
     if row['Receptions'] != '--':
         total += float(row['Receptions'])*0.5
-    if row['Rushing Yards'] != '--':
+    if row['RushingYards'] != '--':
         total += float(row['RushingYards'])*0.1
     if row['RushingTDs'] != '--':
         total += float(row['RushingTDs'])*6
@@ -19,16 +20,6 @@ def halfPPRFantasyPoints(row):
         total -= float(row['FumblesLost'])*2
     return total
 
-
-def getNextGameScore(row, data):
-    id = row['PlayerId']
-    year = row['Year']
-    week = row['Week'] + 1
-    playerSet = data[data.PlayerId == id]
-    playerSeason = playerSet[playerSet.Year == year]
-    nextGame = playerSeason[playerSeason.Week == week]
-    score = nextGame['halfPPR']
-    return score
 
 
 data = pd.read_csv('/Users/patrickbutler/documents/NFLDataScience/nflstatistics/rec_game_logs.csv')
@@ -41,13 +32,14 @@ data.columns = [c.replace(' ', '') for c in data.columns]
 data = data[data.Year >= 2004]
 data = data[data.Season == "Regular Season"]
 
-data = data.sort(['PlayerId', 'GameDate'])
-
 # Calculate fantasy points in half PPR for each game log
-#data['halfPPR'] = data.apply (lambda row: halfPPRFantasyPoints (row), axis=1)
+data['halfPPR'] = data.apply (lambda row: halfPPRFantasyPoints (row), axis=1)
 
-print(getNextGameScore(pd.Series(data.iloc[[12]]), data))
-# We now need a way to create y - the fantasy points scored in that player's next game
-#data['nextGamePoints'] = data.apply (lambda row: getNextGameScores(row, data), axis = 1)
+# Now look at the next row to get the fantasy scpre for the next game
+data['nextGameScore'] = data['halfPPR'].shift(-1)
 
-print(data.head(5))
+# Remove the last game for each player (Where the "next" score is from another player)
+data['nextRowPlayer'] = data['PlayerId'].shift(-1)
+data = data[data.PlayerId == data.nextRowPlayer]
+
+train, test = train_test_split(data, test_size=0.2)
